d11dd3ddd6aae593cfdf0841e70f3b03079b0b63
Arielle Levi <arielle.levi@epita.fr>
2020-04-07 23:33:03 +0200

    feat(preprocessing): work on preprocessing

--- preprocess.py
+++ preprocess.py
@@ -5,11 +5,11 @@ def line_detection(img):
     cv2.imshow("img", img)
     cv2.waitKey(0)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    low_yellow = np.array([18, 94, 140]);
-    high_yellow = np.array([48, 255, 255]);
     cv2.imshow("hsv", hsv)
     cv2.waitKey(0)
 
+    low_yellow = np.array([0, 0, 150]);
+    high_yellow = np.array([255, 50, 255]);
     mask = cv2.inRange(hsv, low_yellow, high_yellow)
     cv2.imshow("mask", mask)
     cv2.waitKey(0)
@@ -18,7 +18,7 @@ def line_detection(img):
     cv2.imshow("edge", edge)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    lines = cv2.HoughLinesP(edge, 1, np.pi / 180, 50, 100, 10)
+    lines = cv2.HoughLinesP(edge, 1, np.pi / 180, 100, 50)
     if lines is None:
         return
     for line in lines:
@@ -28,9 +28,9 @@ def line_detection(img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-img1 = cv2.imread('frame_40_gas_0.4_dir_0.0.jpg')
-img2 = cv2.imread('frame_122_gas_0.4_dir_-0.27542755839822025.jpg')
-img3 = cv2.imread('frame_281_gas_0.4_dir_-0.1778642936596218.jpg')
+img1 = cv2.imread('road1.png')
+img2 = cv2.imread('road2.png')
+img3 = cv2.imread('road3.png')
 
 line_detection(img1)
 line_detection(img2)
